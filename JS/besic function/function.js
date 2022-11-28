730515c92a6510cf3bace44ca8b73c545976929b
rahulvankar <vankarrahul2710@gmail.com>
2022-11-28 12:59:19 +0530

    animeshn

---
+++ JS/besic function/function.js
@@ -0,0 +1,10 @@
+function CookMaggin(maggi, pani, tapeli ){
+    console.log("Your maggie will be ready in "
+    + maggi * 2
+    + " minutes "
+    + " and Ingradients used are:- "
+    + maggi + "Maggi"
+    + pani + "cups of water"
+    + "using " + tapeli + "pan")
+}
+CookMaggin(4 , 8 , 1);
