15621c59d99fc2c65bfd491f0e2fd00b7c4a8098
Rahulvankar <vankarrahul2710@gmail.com>
2023-02-25 09:04:20 +0530

    psd5

---
+++ psd to html/css/chepter3.py
@@ -0,0 +1,7 @@
+story = "sagar is good. \n\"he is very good.\" "
+print(story)
+st = " this is a string with double spaces ok"
+st = st.replace(" ","  ")
+print(st)
+letter = "dear sagar, \n\tthis is python course is nice! \nthanks!"
+print(letter)
