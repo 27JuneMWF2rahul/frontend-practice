27bf3f17c4112b21be0d86eef45f43d57bb60ad8
Rahulvankar <vankarrhul2710@gmail.com>
2023-07-13 14:56:40 -0700

    nots

--- js work/notsapp/js/note.js
+++ js work/notsapp/js/note.js
@@ -6,7 +6,12 @@ createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable","true");
+    inputBox.setAttribute("contenteditable", "true");
     img.src = "img/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+})
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove()
+    }
 })
