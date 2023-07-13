5854f35f9fef32150afa53a5888c770dae90bf2a
Rahulvankar <vankarrhul2710@gmail.com>
2023-07-13 15:33:46 -0700

    nots

--- js work/notsapp/js/note.js
+++ js work/notsapp/js/note.js
@@ -2,16 +2,33 @@ const notesContainer =  document.querySelector(".notes-container");
 const createBtn =  document.querySelector(".btn");
 const notes =  document.querySelector(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes()
+function updateStorage(){
+    localStorage.setItem("notes" , notesContainer.innerHTML);
+}
 createBtn.addEventListener("click", ()=>{
-    let inputBox = document.createElement("p");
+    let inputBox = document.createElement("P");
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
     img.src = "img/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 })
-notesContainer.addEventListener("click",function(e){
+notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
-        e.target.parentElement.remove()
+        e.target.parentElement.remove();
+        updateStorage();
     }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+
 })
